e2cc989d4a0ad023645aeba8acd115746fff434b
rxOred <rxored@gmail.com>
2023-07-16 00:04:30 +0530

    routers

---
+++ api/v1/router/router.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
+)
+
+func SetupRoutes() *chi.Mux {
+	r := chi.NewRouter()
+	r.Mount("/user", User{}.Routes())
+	r.Mount("/book", Book{}, Routes())
+	r.Mount("/author", Author{}.Routes())
+	r.Mount("/requests", Requests{}.Routes())
+}
