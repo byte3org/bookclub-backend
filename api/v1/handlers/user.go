80aabd37fcd541088087a1a5f6b261a218bca3d0
rxOred <rxored@gmail.com>
2023-07-16 01:02:50 +0530

    user handler

---
+++ api/v1/handlers/user.go
@@ -0,0 +1,23 @@
+package userhandler
+
+import "net/http"
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func AuthenticateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetUserDetails(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
