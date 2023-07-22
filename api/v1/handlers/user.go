6a424344a9b3076f34a473c2e6ac0e93bca3c785
rxOred <rxored@gmail.com>
2023-07-23 01:24:21 +0530

    feat: added user handlers

--- api/v1/handlers/user.go
+++ api/v1/handlers/user.go
@@ -1,40 +1,107 @@
 package userhandler
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/byte3/bookclub/backend/helpers"
+	"github.com/byte3/bookclub/backend/helpers/jwt"
 	"github.com/byte3/bookclub/backend/internal/database"
+	"github.com/byte3/bookclub/backend/internal/models"
 )
 
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	users, err := database.SelectAllUsers()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(&users)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	username := r.Form.Get("username")
-	email := r.Form.Get("email")
-	password := r.Form.Get("password")
+	w.Header().Set("Content-Type", "application/json")
 
-	hashed_password, err := helpers.HashPassword(password)
+	if r.Header.Get("Content-Type") != "application/json" {
+		msg := "Content-Type header is not application/json"
+		http.Error(w, msg, http.StatusUnsupportedMediaType)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var user models.User
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user.Password, err = helpers.HashPassword(user.Password)
 	if err != nil {
 		http.Error(w, "password cannot be stored", http.StatusInternalServerError)
 		return
 	}
 
-	database.InsertUser(username, email, hashed_password)
+	database.InsertUser(&user)
+	msg := map[string]interface{}{
+		"id":   user.Id,
+		"name": user.Username,
+	}
+
+	j, err := json.Marshall(msg)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte("user created"))
+	w.Write([]byte(j))
 	return
 }
 
 func AuthenticateUser(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	username := r.Form.Get("username")
-	password := r.Form.Get("password")
+	w.Header().Set("Content-Type", "application/json")
 
-	hashed_password, err := helpers.HashPassword(password)
+	if r.Header.Get("Content-Type") != "application/json" {
+		msg := "Content-Type header is not application/json"
+		http.Error(w, msg, http.StatusUnsupportedMediaType)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	u := struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}{}
+
+	err = json.Unmarshal(body, &u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	hashed_password, err := helpers.HashPassword(u.Password)
 	if err != nil {
 		http.Error(w, "password cannot be stored", http.StatusInternalServerError)
 		return
@@ -42,28 +109,48 @@ func AuthenticateUser(w http.ResponseWriter, r *http.Request) {
 
 	// check if user is in the database
 	// search for username in the db
-	user, err := database.SelectUserbyName(username)
+	user, err := database.SelectUserbyName(u.Username)
 	if err != nil {
 		http.Error(w, "user not found", http.StatusBadRequest)
 		return
 	}
 
-	if user.Username != username || user.Password != hashed_password {
+	if user.Username != u.Username || user.Password != hashed_password {
 		http.Error(w, "authentication failed", http.StatusBadRequest)
 		return
 	}
 
+	token_str := jwt.GenerateToken(user.Username, user.Email, user.Password)
+	msg := map[string]interface{}{
+		"status": "user authenticated",
+		"token":  token_str,
+	}
+
 	w.WriteHeader(200)
-	w.Write([]byte("user authenticated"))
-
-	// write auth token
-	w.Write([]byte())
-
+	w.Write([]byte(msg))
 	return
 }
 
 func GetUserDetails(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	id, err := strconv.Atoi(r.Context().Value("id").(string))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	user, err := database.GetUserDetails(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	b, err := json.Marshal(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+	return
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
--- api/v1/middlewares/auth.go
+++ api/v1/middlewares/auth.go
@@ -1,47 +1,29 @@
 package middlewares
 
 import (
-	"context"
 	"net/http"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/go-chi/jwtauth"
+	"github.com/lestrrat-go/jwx/jwt"
 )
 
-func ensureAdminAuth(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header["Token"] != nil {
-			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
-				_, ok := token.Method.(*jwt.SigningMethodECDSA)
-				if !ok {
-					w.WriteHeader(http.StatusUnauthorized)
-					_, err := w.Write([]byte("token sign method is invalid"))
-					if err != nil {
-						return nil, err
-					}
-				}
-				return "", nil
-			})
-			if err != nil {
-				w.WriteHeader(http.StatusUnauthorized)
-				_, err := w.Write([]byte("error with jwt"))
-				if err != nil {
-					return
-				}
-			}
+func EnsureAdminAuth(next http.Handler) http.Handler {
 
-			ctx := context.WithValue(r.Context(), "authenticated", "admin")
-
-			if token.Valid {
-				next.ServeHTTP(w, r.WithContext(ctx))
-			}
-		}
-	})
 }
 
-func ensureAuth(next http.Handler) http.Handler {
+func EnsureAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header["Token"] != nil {
+		token, _, err := jwtauth.FromContext(r.Context())
 
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
 		}
+
+		if token == nil || jwt.Validate(token) != nil {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
 	})
 }
