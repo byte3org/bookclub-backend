acca47ecc6454b9f94dfff54c3e29ec72fe00b71
rxOred <rxored@gmail.com>
2023-07-23 01:24:36 +0530

    feat: added context middleware

---
+++ api/v1/middlewares/context.go
@@ -0,0 +1,22 @@
+package middlewares
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func UserCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "id", chi.URLParam(r, "id"))
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func RequestCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "id", chi.URLParam(r, "id"))
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
