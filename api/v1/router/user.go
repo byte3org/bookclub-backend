14e9707e3d2878dc42ee4498b44b2eadd9d18747
rxOred <rxored@gmail.com>
2023-07-23 01:25:11 +0530

    feat: added user router

--- api/v1/router/user.go
+++ api/v1/router/user.go
@@ -1,6 +1,12 @@
 package router
 
-import "github.com/go-chi/chi"
+import (
+	"github.com/byte3/bookclub/backend/api/v1/handlers/userhandler"
+	"github.com/byte3/bookclub/backend/api/v1/middlewares"
+	"github.com/byte3/bookclub/backend/helpers/jwt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/jwtauth"
+)
 
 type User struct{}
 
@@ -9,19 +15,25 @@ func (u User) Routes() chi.Router {
 
 	// these endpoints are only available to admin service
 	r.Group(func(r chi.Router) {
-		r.Use(ensureAdminAuth)
+		r.Use(jwtauth.Verifier(jwt.TokenAuth))
+		r.Use(middlewares.EnsureAdminAuth)
 		// for now only admin authenticated users can get all the users
 		r.Get("/", userhandler.GetAllUsers)
+		r.Get("/count", userhandler.GetUserCount)
 	})
 
-	r.Post("/register", userhandler.CreateUser)
-	r.Post("/login", userhandler.AuthenticateUser)
+	r.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(jwt.TokenAuth))
+		r.Post("/register", userhandler.CreateUser)
+		r.Post("/login", userhandler.AuthenticateUser)
+	})
 
 	// these endpoints are only available to authenticated users
 	r.Group(func(r chi.Router) {
-		r.Use(ensureAuth)
+		r.Use(jwtauth.Verifier(jwt.TokenAuth))
+		r.Use(middlewares.EnsureAuth)
 		r.Route("/{id}", func(r chi.Router) {
-			r.Use(UserCtx)
+			r.Use(middlewares.UserCtx)
 			r.Get("/", userhandler.GetUserDetails)
 			r.Delete("/", userhandler.DeleteUser)
 		})
