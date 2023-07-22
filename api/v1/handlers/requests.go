910b1e2e5b10e7d45a2f2de26ed01941dfd3fdde
rxOred <rxored@gmail.com>
2023-07-23 01:23:51 +0530

    feat: added request handlers

---
+++ api/v1/handlers/requests.go
@@ -0,0 +1,31 @@
+package requestshandler
+
+import "net/http"
+
+func GetAllRequests(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateRequest(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetAllRequestsCount(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetAllPendingRequests(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetAllAcceptedRequests(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetUserDetails(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
