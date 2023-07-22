78ac9dd8fe7a667f4ed82b64559a2cde47625dc2
rxOred <rxored@gmail.com>
2023-07-23 01:24:52 +0530

    feat: added request router

---
+++ api/v1/router/requests.go
@@ -0,0 +1,34 @@
+package router
+
+import (
+	"github.com/byte3/bookclub/backend/api/v1/middlewares"
+	"github.com/byte3/bookclub/backend/api/v1/router/requestshandler"
+	"github.com/byte3/bookclub/backend/helpers/jwt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth"
+)
+
+type Request struct{}
+
+func (re Request) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	// these endpoints are only available to admin service
+	r.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(jwt.TokenAuth))
+		r.Get("/", requestshandler.GetAllRequests)
+		r.Post("/", requestshandler.CreateRequest)
+		r.Get("/count", requestshandler.GetAllRequestsCount)
+		r.Get("/status/pending", requestshandler.GetAllPendingRequests)
+		r.Get("/status/accepted", requestshandler.GetAllAcceptedRequests)
+	})
+
+	r.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(jwt.TokenAuth))
+		r.Route("/{id}", func(r chi.Router) {
+			r.Use(middlewares.RequestCtx)
+			r.Get("/", requestshandler.GetUserDetails)
+			r.Delete("/", requestshandler.DeleteUser)
+		})
+	})
+}
