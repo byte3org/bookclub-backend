138cb36af81ce6a9c1365fe2a100a41c426e376d
rxOred <rxored@gmail.com>
2023-09-12 02:23:04 +0530

    feat: request routes and handlers

--- api/v1/router/requests.go
+++ api/v1/router/requests.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+    "net/http"
 	requestshandler "github.com/byte3/bookclub/backend/api/v1/handlers"
 	"github.com/byte3/bookclub/backend/api/v1/middlewares"
 	"github.com/go-chi/chi/v5"
@@ -11,19 +12,19 @@ type Request struct{}
 func (re Request) Routes() chi.Router {
 	r := chi.NewRouter()
 
-	r.Group(func(r chi.Router) {
-		r.Get("/", requestshandler.GetAllRequests)
-		r.Post("/", requestshandler.CreateRequest)
-		r.Get("/count", requestshandler.GetAllRequestsCount)
-		r.Get("/status/pending", requestshandler.GetAllPendingRequests)
-		r.Get("/status/accepted", requestshandler.GetAllAcceptedRequests)
+	r.Group(func(rootRequestRoutes chi.Router) {
+		rootRequestRoutes.Get("/", requestshandler.GetAllRequests)
+		rootRequestRoutes.Post("/", requestshandler.CreateRequest)
+		rootRequestRoutes.Get("/count", requestshandler.GetAllRequestsCount)
+		rootRequestRoutes.Get("/status/pending", requestshandler.GetAllPendingRequests)
+		rootRequestRoutes.Get("/status/accepted", requestshandler.GetAllAcceptedRequests)
 	})
 
-	r.Group(func(r chi.Router) {
-		r.Route("/{id}", func(r chi.Router) {
-			r.Use(middlewares.RequestCtx)
-			r.Get("/", requestshandler.GetRequestDetails)
-			r.Get("/", requestshandler.DeleteRequest)
+	r.Group(func(resourceRequestRoutes chi.Router) {
+		resourceRequestRoutes.Route("/{id}", func(req  chi.Router) {
+			req.Use(middlewares.RequestCtx)
+			req.Get("/", requestshandler.GetRequestDetails)
+			req.Delete("/", requestshandler.DeleteRequest)
 		})
 	})
 
