914bf7313ef11c32478f0f65ec918c6ab7001a20
rxOred <rxored@gmail.com>
2023-09-12 02:39:51 +0530

    feat: added middleware to handle request and response content types

---
+++ api/v1/middlewares/content.go
@@ -0,0 +1,21 @@
+package middlewares
+
+import "net/http"
+
+func CheckContentType(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        if r.Header.Get("Content-Type") != "application/json" {
+            msg := "Content-Type header is not application/json"
+            http.Error(w, msg, http.StatusUnsupportedMediaType)
+            return
+        } 
+        next.ServeHTTP(w, r)
+    }) 
+} 
+
+func SetWriterContentType(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "application/json")
+        next.ServeHTTP(w, r)
+    })
+}
