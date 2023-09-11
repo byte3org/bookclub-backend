4ac199c393c44446ed18420108acaf9adceb5767
rxOred <rxored@gmail.com>
2023-09-11 18:59:41 +0530

    feat: removed user related queries

--- internal/database/database.go
+++ internal/database/database.go
@@ -15,8 +15,8 @@ var Db *gorm.DB
 
 func Initialize(config *config.Config) {
 	var err error
-	dsn := "host=localhost user=rxored dbname=bookclub port=9920 sslmode=disable TimeZone=Asia/Colombo"
-	Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    println(config.DbString)
+	Db, err = gorm.Open(postgres.Open(config.DbString), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -35,36 +35,9 @@ func Initialize(config *config.Config) {
 		&models.BookRequestDeclined{},
 		&models.BookRequestStatus{}, &models.BookReturns{},
 		&models.ISBNVersion{},
-		&models.User{},
-		&models.UserAddressInfo{},
-		&models.UserContactInfo{},
 	); err != nil {
 		log.Fatal(err.Error())
 	}
-}
-
-func SelectAllUsers() ([]models.User, error) {
-	users := []models.User{}
-	result := Db.Find(&users)
-	return users, result.Error
-}
-
-func SelectUserbyName(username string) (models.User, error) {
-	user := models.User{}
-	result := Db.Where("username = ?", username).First(&user)
-	return user, result.Error
-}
-
-func InsertUser(user *models.User) (int, error) {
-	result := Db.Create(user)
-
-	return int(result.RowsAffected), result.Error
-}
-
-func SelectUserById(id int) (models.User, error) {
-	user := models.User{}
-	result := Db.First(&user, id)
-	return user, result.Error
 }
 
 func SelectAllRequests() ([]models.BookRequest, error) {
