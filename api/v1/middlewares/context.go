e6aa5dd4a59958d0349886a047d2d561dd904a41
rxOred <rxored@gmail.com>
2023-09-11 19:00:33 +0530

    feat: removed user extractor

--- api/v1/middlewares/context.go
+++ api/v1/middlewares/context.go
@@ -7,13 +7,6 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func UserCtx(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.WithValue(r.Context(), "id", chi.URLParam(r, "id"))
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
 func RequestCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.WithValue(r.Context(), "id", chi.URLParam(r, "id"))
