0ae8555b27b21737075829bbd3d500b766dfef5d
rxOred <rxored@gmail.com>
2023-09-13 21:46:44 +0530

    feat: added userid extract middleware

--- api/v1/middlewares/oidc.go
+++ api/v1/middlewares/oidc.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"net/http"
 	"strings"
+
+	"github.com/byte3/bookclub/backend/config"
+	"github.com/byte3/bookclub/backend/helpers"
 )
 
 func ExtractOidcToken(next http.Handler) http.Handler {
@@ -23,5 +26,30 @@ func ExtractUserIdfromOidc(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         // extract oidc token from request context
         // do the client things
+        token := r.Context().Value("token").(string)
+        
+        client := http.Client{}
+        req, err := http.NewRequest("GET", config.GetConfig().BookclubAuth, nil)    
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        req.Header = http.Header{
+            "Authorization" : {"Bearer " + token},
+        }
+        res, err := client.Do(req)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        defer res.Body.Close()
+        jsonRes, err := helpers.ParseJsonBody(res.Body)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        userId := jsonRes["sub"].(int)
+        ctx := context.WithValue(r.Context(), "user_id", userId)
+        next.ServeHTTP(w, r.WithContext(ctx))
     })
 }
